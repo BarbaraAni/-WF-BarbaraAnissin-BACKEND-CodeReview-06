c14a214e55e2f4792e7757f286979b43d1fdd7be
Barbara Anissin <anissinb@gmail.com>
2020-01-10 09:56:00 +0100

    jdbc added

--- src/sql/report.sql
+++ src/sql/report.sql
@@ -22,9 +22,8 @@ INNER  JOIN `class` on fk_class_id =  class_id
 WHERE class_id = 2
 
 
---(10) add n-to-m relation to database that saves the data of type “single teacher can teach several classes. Save the info of the type, what teachers are teaching what classes”
---Notes:
---use phpmyadmin to export the database into a file “database-dump.sql”.
+--(10) add n-to-m relation to database that saves the data of type “single teacher can teach several classes.
+--Save the info of the type, what teachers are teaching what classes”
 --Create a JavaFX & JDBC based project in Intellij, add WHOLE project to the GIT. Then, add a folder “sql” in the project structure, and add files “report.sql” and “database-dump.sql” to the GIT.
 --(20) JavaFX: Display the list of the teachers in a ListView, showing the teacher’s data as Surname, Name in the ListView . Once a teacher’s name is selected, you need to populate teacher’s data in the field to the left (see the mockup below)
 --Note: All the data are fetched from the database on the start of the application.
